9641132615c0a727b4878b0c54b95ab528cb6c0c
Vincent Msi <msika.vincent@gmail.com>
2021-02-13 15:55:08 +0900

    Made requested changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,6 @@ require 'open-uri'
 url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=train_station&location=35.681382,139.76608399999998&radius=30000&key=AIzaSyCwIpzQNIclviTDcD_ivXtEfD4RYwcppj4"
 stations = open(url).read
 station_list = JSON.parse(stations)
-station_name = station_list['results'].each do |station|
-  station['name']
-end
 
 puts "Deleting everything"
 User.destroy_all
