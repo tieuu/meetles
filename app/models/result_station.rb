e0638f94d7a589d40432688025fd3eb38725c7de
Tieu Linh Dong <linhdongtieu@gmail.com>
2021-02-20 13:50:47 +0900

    added restaurants

--- app/models/result_station.rb
+++ app/models/result_station.rb
@@ -1,5 +1,30 @@
+require 'open-uri'
+require 'nokogiri'
+require 'json'
+
 class ResultStation < ApplicationRecord
   belongs_to :station
   belongs_to :meetle
   has_many :fares
+
+  def self.location(station)
+    url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=#{station}%20station&inputtype=textquery&fields=formatted_address,name,geometry&key=AIzaSyCwIpzQNIclviTDcD_ivXtEfD4RYwcppj4"
+
+    html_file = open(url).read
+    html_doc = JSON.parse(html_file)
+
+    location = html_doc['candidates'].first['geometry']['location']
+    return "#{location['lat']},#{location['lng']}"
+  end
+
+  def self.restaurant(station)
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{location(station)}&radius=1500&type=restaurant&key=AIzaSyCwIpzQNIclviTDcD_ivXtEfD4RYwcppj4"
+
+    html_file = open(url).read
+    html_doc = JSON.parse(html_file)
+
+    restaurants = []
+    html_doc['results'].first(3).each { |result| restaurants << result['name'] }
+    return restaurants
+  end
 end
