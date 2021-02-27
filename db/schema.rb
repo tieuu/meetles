1a167ab949d9e85730d68565b077adb4d006c457
Dan Ring <dan.ring@me.com>
2021-02-27 15:54:58 +0900

    updated show page to remove issues

--- db/schema.rb
+++ db/schema.rb
@@ -78,7 +78,6 @@ ActiveRecord::Schema.define(version: 2021_02_27_052356) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "username"
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
