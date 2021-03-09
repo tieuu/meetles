e1324b056441f0604b4317a3914443fdb638e2ed
Vincent Msi <msika.vincent@gmail.com>
2021-03-09 22:39:14 +0900

    Better markers, bigger map, profile pic

--- db/schema.rb
+++ db/schema.rb
@@ -117,7 +117,6 @@ ActiveRecord::Schema.define(version: 2021_03_04_122304) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "username"
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
